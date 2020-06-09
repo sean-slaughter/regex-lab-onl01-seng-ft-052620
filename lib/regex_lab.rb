aea4711c1f068fda203d82e435d1e38accef62ca
sean-slaughter <sthompson2822@gmail.com>
2020-06-09 11:50:10 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    if(word.match(/^[aeiouAEIOU]/))
+        true
+    else
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w*[ing{3}]/)
 end
 
 def words_five_letters_long(text)
-
+    array = text.split(" ")
+    array.grep(/\A\w{5}\z/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if(text.match(/^[A-Z]/) && text.match(/[!?.'";,:]\z/))
+        true
+    else
+        false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    if(phone.match(/\d{3}.?\d{3}.?\d{4}/))
+        true
+    else
+        false
+    end
 end
